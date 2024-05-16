ac8b583fee3c8b1a6118408ebf43a1f2ef2c13e5
Dhruva Nagaraj <dhruva.nagaraj@siemens.com>
2024-05-16 13:57:50 +0530

    trying again

---
+++ pkgs/contact-manager/contacts.go
@@ -0,0 +1,13 @@
+package contact
+
+type Contact struct {
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+	Email     string `json:"Email"`
+}
+
+// TODO Add Contact
+// TODO View Contact
+// TODO Delete Contact
+// TODO Update Contact
+// TODO Search Contact
